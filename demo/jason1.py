5dce8793986d7b4c51593c9469cbc9928642a355
Jason Corso <jjcorso@umich.edu>
2020-03-11 11:54:24 -0400

    running it on a few samples and seeing the results.  not great.

--- demo/jason1.py
+++ demo/jason1.py
@@ -6,6 +6,9 @@
 import sys
 from collections import OrderedDict
 
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+
 import numpy as np
 import torch
 from torch.autograd import Variable
@@ -17,9 +20,12 @@ sys.path.append('../utils/')
 sys.path.append('../data/')
 
 import models.SSD_vgg as SSD
-from data import VOC_300
+from data import BaseTransform, VOC_300
+from data import VOC_CLASSES as labelmap
 from layers.functions import Detect, PriorBox
 from utils.timer import Timer
+
+import eta.core.image as etai
 
 def py_cpu_nms(dets, thresh):
     """Pure Python NMS baseline."""
@@ -70,7 +76,7 @@ class ObjectDetector:
         assert img.shape[2] == 3
         x = Variable(self.transform(img).unsqueeze(0), volatile=True)
         if self.cuda:
-            x = x.cuda()
+            x = x.to("cuda")
         _t['im_detect'].tic()
         out = net(x, test=True)  # forward pass
         boxes, scores = self.detection.forward(out, priors)
@@ -119,8 +125,9 @@ class ObjectDetector:
 img_dim = 300
 num_classes = 21  # the model says it is 7+12, but this is not the class count,
                   #  it is the years of the VOC datasets.  total classes are 21
+cfg = VOC_300
 
-priorbox = PriorBox(VOC_300)
+priorbox = PriorBox(cfg)
 # noting the interpreter message that volatile=True no longer has any effect
 # and it should rather be done with `with torch.no_grad()`
 priors = Variable(priorbox.forward(), volatile=True)
@@ -145,9 +152,59 @@ for k, v in state_dict.items():
     new_state_dict[name] = v
 net.load_state_dict(new_state_dict)
 net.eval()
+net.to("cuda")
 
 print("finished loading the model")
 print(net)
+print("net.size is %s" % net.size)  # just outputs 300 flat (unsure if this is
+# torch field or only in this library
+
+detector = Detect(num_classes, 0, cfg)
+
+rgb_means = (104, 117, 123)
+rgb_std = (1, 1, 1)
+
+# this is the data transform to map raw image into the one the net needs
+#  this needs to be rewritten.
+# note that the last part is just swapping axes so that HWC --> CHW
+transform = BaseTransform(net.size, rgb_means, rgb_std, (2, 0, 1))
+object_detector = ObjectDetector(net, detector, transform, num_classes, True)
+
+print('Loading image..')
+# the baseTransform in this library will do all the resizing and datatype
+# conversion
+#image = etai.read('http://images.cocodataset.org/val2017/000000252219.jpg')
+image = etai.read('http://images.cocodataset.org/val2017/000000397133.jpg')
+#image = etai.read('http://images.cocodataset.org/val2017/000000037777.jpg')
+
+print('running the prediction')
+with torch.no_grad():
+    detect_bboxes = object_detector.predict(image)
+
+print('detections done')
+print(detect_bboxes)
+
+for class_id, class_collection in enumerate(detect_bboxes):
+    print("[%02d] %s --> %s" % (class_id, labelmap[class_id],
+                                len(class_collection)))
+
+
+fig, ax = plt.subplots(1)
+ax.imshow(image)
+
+for class_id, class_collection in enumerate(detect_bboxes):
+    if len(class_collection) > 0:
+        print("plotting %s" % labelmap[class_id])
+        for i in range(class_collection.shape[0]):
+            sx, sy, ex, ey, conf  = class_collection[i]
+            print(sx, sy, ex, ey, conf)
+            x, y, w, h = sx, sy, ex - sx, ey - sy
+            rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor="r",
+                                     facecolor="none")
+            ax.add_patch(rect)
+            ax.text(x, y, "{} {:.0f}%".format(labelmap[class_id], conf*100))
+
+plt.show()
 
 
 
